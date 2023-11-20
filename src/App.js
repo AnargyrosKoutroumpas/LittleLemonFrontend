61fac25d853f1da7ef9cd324e4b17d2cf2c8e921
AnargyrosKoutroumpas <ak@cleopatrassponnges.com>
2023-11-20 18:23:17 +0200

    Added OG meta tags and semantic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <img src="" alt=""></img>
+      {/* copyright note */}
+    </footer>
+  );
+}
+
+export default Footer;
